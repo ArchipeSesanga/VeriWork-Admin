f5bc32563767ac2148a46651f9e2839159d9f499
Archipe Sesanga <moisemozess@gmail.com>
2025-09-12 14:20:22 +0200

    initial .net files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VeriWork_Admin.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
